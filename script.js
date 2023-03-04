6a9010247362277e0b0543f04a3803b5c4e2df94
Ruan Reis <ruancosme23@gmail.com>
2023-03-04 05:06:48 -0300

    fase incial finalizada

---
+++ script.js
@@ -0,0 +1,37 @@
+const olhos = [...document.getElementsByClassName("olho")];
+
+let posX_mouse = 0;  
+let posY_mouse = 0;
+
+window.addEventListener("mousemove", (evento) => {
+    posX_mouse = evento.clientX;
+    posY_mouse = evento.clientY;
+    // console.log(posX_mouse);;// para ver a posição do mouse.
+
+    const rotacao = Math.atan2(posY_mouse,posX_mouse) * 180 / Math.PI
+
+    olhos.forEach((o) => {
+       o.style.transform = "rotate("+rotacao+"deg)" 
+    });
+});
+
+
+
+const p1 = document.getElementById("p1").addEventListener("click", () => {
+    alert("Porta 1");
+});
+
+
+const p2 = document.getElementById("p2").addEventListener("click", () => {
+    alert("Porta 2");
+});
+
+
+const p3 = document.getElementById("p3").addEventListener("click", () => {
+    alert("Porta 3");
+});
+
+
+
+
+
