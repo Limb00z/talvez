4427a4b40f6d94f82ac97e120dafaa6d9a87276e
Ruan Reis <ruancosme23@gmail.com>
2023-03-05 00:19:05 -0300

    poucas mudanças

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const olhos = [...document.getElementsByClassName("olho")];
 
-let som = new Audio("psicose.mp3");
+var som = new Audio("psicose.mp3");
 som.play();
 
-
 window.addEventListener("mousemove", (evento) => {
+    
     let posX_mouse = evento.clientX;
     let posY_mouse = evento.clientY;
     // console.log(posX_mouse);;// para ver a posição do mouse.
@@ -19,34 +19,51 @@ window.addEventListener("mousemove", (evento) => {
 
 
 const p1 = document.getElementById("p1").addEventListener("click", () => {
-    Swal.fire({
-        title: 'Você caiu num bug x.x',
-        width: 600,
-        padding: '3em',
-        color: '#716add',
-        background: '#fff url(https://sweetalert2.github.io/#frameworks-integrationsimages/trees.png)',
-        backdrop: `
+  
+Swal.fire({
+    title: 'Você caiu num bug x.x',
+    width: 600,
+    padding: '3em',
+    color: '#716add',
+    background: '#fff url(https://sweetalert2.github.io/#frameworks-integrationsimages/trees.png)',
+    backdrop: `
           url("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExODYzYmQ0ZWNhYTAxMGNlYjg2NGQzMGJkMWY3YjI5ODZmYjAwZDg0MCZjdD1n/3o6wrdG8vt4X86Pauc/giphy.gif")    
         `
-      })
-    som.pause();
+});
+     
+  let principal = document.getElementById("principal");
+  principal.innerHTML = "";
+  principal.innerHTML = '<image src="https://i.giphy.com/media/H6W9H29kVsUI2hJE90/giphy.webp" width="100%" height="100%" style="position:absolute" frameBorder="0" allowFullScreen></image>';
+
+
+  
+  
+    // som.pause();
 });
 
 
 const p2 = document.getElementById("p2").addEventListener("click", () => {
-    Swal.fire({
-        icon: 'info',
-        title: 'Apresentação pessoal',
-        text: 'Você será direcionado para minha página pessoal, basta clicar no link abaixo',
-        footer: '<a href="https://limb00z.github.io/Potifolio_novo/" target="_blank">Pórtifólio</a>'
-      })
-    som.pause();
+  Swal.fire({
+    title: '<strong>HTML <u>example</u></strong>',
+    icon: 'info',
+    html:
+      'You can use <b>bold text</b>, ' +
+      '<a href="//sweetalert2.github.io">links</a> ' +
+      'and other HTML tags',
+    showCloseButton: true
+  });
+
+  setTimeout(() => {
+    window.location.href = "https://limb00z.github.io/Potifolio_novo/"
+  }, 5000);
+
+    // som.pause();
 });
 
 
 const p3 = document.getElementById("p3").addEventListener("click", () => {
     alert("p3");
-    som.pause();
+    // som.pause();
 });
 
 
